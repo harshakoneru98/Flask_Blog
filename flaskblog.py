73de38c66524db73ee58002511aea90cd3d25e03
Harsha Vardhan Koneru <harshavardhankoneru1998@gmail.com>
2021-03-24 13:36:38 +0530

    Passing variable into templates

--- flaskblog.py
+++ flaskblog.py
@@ -2,15 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Harsha Vardhan Koneru',
+        'title' : 'Blog post 1',
+        'content' : 'First post content',
+        'date_posted' : 'May 2, 2021'  
+    },
+    {
+        'author' : 'Vamsi Krishna Koneru',
+        'title' : 'Blog post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'January 30, 2021'  
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == '__main__':
